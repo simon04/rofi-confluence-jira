09a734a370ff7cce1b955b54c77a562db8b92b76
Simon Legner <Simon.Legner@gmail.com>
2022-02-14 12:04:42 +0100

    Reformat Python code using Black
    https://github.com/psf/black

--- rofi-confluence-jira.py
+++ rofi-confluence-jira.py
@@ -10,63 +10,66 @@ import webbrowser
 
 class Confluence:
     def __init__(self, url, auth):
-        self.url = url.rstrip('/')
+        self.url = url.rstrip("/")
         self.auth = auth
 
     def fetch(self, limit=500, start=0):
         confluence = requests.get(
-            f'{self.url}/rest/api/content/search',
+            f"{self.url}/rest/api/content/search",
             params={
-                'cql': 'type=page order by lastmodified desc',
-                'limit': limit,
-                'start': start,
+                "cql": "type=page ORDER BY lastmodified DESC",
+                "limit": limit,
+                "start": start,
             },
-            headers={'Content-Type': 'application/json'},
-            auth=self.auth)
-        pages = confluence.json()['results']
+            headers={"Content-Type": "application/json"},
+            auth=self.auth,
+        )
+        pages = confluence.json()["results"]
         for page in pages:
-            space = page['_expandable']['space'].split('/')[-1]
-            title = page['title']
-            url = page['_links']['webui'].strip("/")
-            page['url'] = f'{self.url}/{url}'
-            page['label'] = f'[{space}] {title}'
+            space = page["_expandable"]["space"].split("/")[-1]
+            title = page["title"]
+            url = page["_links"]["webui"].strip("/")
+            page["url"] = f"{self.url}/{url}"
+            page["label"] = f"[{space}] {title}"
         return pages
 
 
 class Jira:
     def __init__(self, url, auth):
-        self.url = url.rstrip('/')
+        self.url = url.rstrip("/")
         self.auth = auth
 
     def fetch(self, limit=1000, start=0):
         jira = requests.get(
-            f'{self.url}/rest/api/2/search',
+            f"{self.url}/rest/api/2/search",
             params={
-                'jql': 'ORDER BY updated',
-                'fields': 'summary',
-                'maxResults': limit,
-                'startAt': start,
+                "jql": "ORDER BY updated",
+                "fields": "summary",
+                "maxResults": limit,
+                "startAt": start,
             },
-            headers={'Content-Type': 'application/json'},
-            auth=self.auth)
-        issues = jira.json()['issues']
+            headers={"Content-Type": "application/json"},
+            auth=self.auth,
+        )
+        issues = jira.json()["issues"]
         for issue in issues:
-            key = issue['key']
-            summary = issue['fields']['summary']
-            issue['url'] = f'{self.url}/browse/{key}'
-            issue['label'] = f'[{key}] {summary}'
+            key = issue["key"]
+            summary = issue["fields"]["summary"]
+            issue["url"] = f"{self.url}/browse/{key}"
+            issue["label"] = f"[{key}] {summary}"
         return issues
 
 
 class Rofi:
     @classmethod
     def show_menu(cls, items, prompt):
-        rofi_input = '\n'.join(items)
+        rofi_input = "\n".join(items)
         rofi = subprocess.run(
-            ['rofi', '-dmenu', '-p', prompt, '-format', 'i', '-i'],
+            ["rofi", "-dmenu", "-p", prompt, "-format", "i", "-i"],
             input=rofi_input,
             stdout=subprocess.PIPE,
-            universal_newlines=True)
+            universal_newlines=True,
+        )
         rofi_key = rofi.returncode
         if rofi_key >= 0:
             return int(rofi.stdout)
@@ -74,13 +77,14 @@ class Rofi:
             return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config = configparser.ConfigParser()
-    config.read(BaseDirectory.load_config_paths('rofi-confluence-jira.cfg'))
-    confluence = Confluence(config['confluence']['URL'],
-                            (config['confluence']['USER'], config['confluence']['PASS']))
-    jira = Jira(config['jira']['URL'],
-                (config['jira']['USER'], config['jira']['PASS']))
+    config.read(BaseDirectory.load_config_paths("rofi-confluence-jira.cfg"))
+    confluence = Confluence(
+        config["confluence"]["URL"],
+        (config["confluence"]["USER"], config["confluence"]["PASS"]),
+    )
+    jira = Jira(config["jira"]["URL"], (config["jira"]["USER"], config["jira"]["PASS"]))
     with multiprocessing.Pool(2) as pool:
         apply_async = pool.apply_async
         results = [
@@ -91,6 +95,6 @@ if __name__ == '__main__':
         ]
         items = functools.reduce(lambda lst, x: lst + x.get(), results, [])
 
-    index = Rofi.show_menu([i['label'] for i in items], 'Confluence/JIRA')
+    index = Rofi.show_menu([i["label"] for i in items], "Confluence/JIRA")
     if index is not None:
-        webbrowser.open_new_tab(items[index]['url'])
+        webbrowser.open_new_tab(items[index]["url"])
